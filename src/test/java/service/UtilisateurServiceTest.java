2c1da66598b2f24e3f88904c72d6af5a5ede991a
alexis-benoit <alexis.benoit.68@gmail.com>
2023-08-21 22:08:47 +0200

    tests utilisateurs

--- src/test/java/service/UtilisateurServiceTest.java
+++ src/test/java/service/UtilisateurServiceTest.java
@@ -6,10 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
 import repository.UtilisateurRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UtilisateurServiceTest {
 
@@ -18,7 +24,10 @@ class UtilisateurServiceTest {
 
     @InjectMocks
     private UtilisateurService utilisateurService;
-
+    
+    @Spy
+    private UtilisateurService utilisateurServiceSpy;
+    
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
@@ -71,5 +80,91 @@ class UtilisateurServiceTest {
         utilisateurService.supprimerUtilisateur(userId);
 
         verify(utilisateurRepository, times(1)).deleteById(userId);
+    }
+    
+    @Test
+    void testCreerUtilisateurChampManquant() {
+        Utilisateur utilisateur = new Utilisateur();
+
+        doThrow(ConstraintViolationException.class)
+        .when(utilisateurRepository).save(utilisateur);
+
+	    assertThrows(ConstraintViolationException.class, () -> {
+	        utilisateurService.creerUtilisateur(utilisateur);
+	    });
+    }
+    
+    @Test
+    void testMettreAJourUtilisateurChampManquant() {
+        Utilisateur utilisateur = new Utilisateur();
+        // Configurer l'utilisateur avec des champs manquants
+
+        doThrow(ConstraintViolationException.class)
+            .when(utilisateurServiceSpy)
+            .mettreAJourUtilisateur(anyLong(), any(Utilisateur.class));
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            utilisateurServiceSpy.mettreAJourUtilisateur(1L, utilisateur);
+        });
+    }
+    
+    @Test
+    void testCreerUtilisateurLimiteTailleNomUtilisateur() {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNomUtilisateur("123456789012345678901234567890123456789012345678901"); // 51 caractères
+
+        doThrow(ConstraintViolationException.class)
+            .when(utilisateurRepository).save(utilisateur);
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            utilisateurService.creerUtilisateur(utilisateur);
+        });
+    }
+
+    @Test
+    void testCreerUtilisateurLimiteTailleMotDePasse() {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNomUtilisateur("utilisateur1");
+        utilisateur.setMotDePasse("123456789012345678901234567890123456789012345678901234567890123456789012345678901"); // 101 caractères
+
+        doThrow(ConstraintViolationException.class)
+            .when(utilisateurRepository).save(utilisateur);
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            utilisateurService.creerUtilisateur(utilisateur);
+        });
+    }
+
+    @Test
+    void testMettreAJourUtilisateurLimiteTailleNomUtilisateur() {
+        Long userId = 1L;
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setId(userId);
+        utilisateur.setNomUtilisateur("123456789012345678901234567890123456789012345678901"); // 51 caractères
+
+        doThrow(ConstraintViolationException.class)
+            .when(utilisateurServiceSpy)
+            .mettreAJourUtilisateur(userId, utilisateur);
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            utilisateurServiceSpy.mettreAJourUtilisateur(userId, utilisateur);
+        });
+    }
+
+    @Test
+    void testMettreAJourUtilisateurLimiteTailleMotDePasse() {
+        Long userId = 1L;
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setId(userId);
+        utilisateur.setNomUtilisateur("utilisateur1");
+        utilisateur.setMotDePasse("123456789012345678901234567890123456789012345678901234567890123456789012345678901"); // 101 caractères
+
+        doThrow(ConstraintViolationException.class)
+            .when(utilisateurServiceSpy)
+            .mettreAJourUtilisateur(userId, utilisateur);
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            utilisateurServiceSpy.mettreAJourUtilisateur(userId, utilisateur);
+        });
     }
 }
